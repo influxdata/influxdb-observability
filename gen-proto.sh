8ad1b028467ffd0807f946ab8651531a2b65c259
Jacob Marble <jacobmarble@influxdata.com>
2021-04-27 13:18:37 -0700

    chore: switch away from gogo proto

--- gen-proto.sh
+++ gen-proto.sh
@@ -20,7 +20,9 @@ cd "$BASEDIR"/opentelemetry-proto
 find . -type f -name '*.proto' -exec sed -i '' 's+github.com/open-telemetry/opentelemetry-proto/gen/go/+github.com/influxdata/influxdb-observability/otlp/+g' {} +
 find . -type f -name '*.proto' -exec sed -i '' 's+opentelemetry\.proto\.+internal.opentelemetry.proto.+g' {} +
 find . -type f -name '*.yaml' -exec sed -i '' 's+selector: opentelemetry\.proto\.+selector: internal.opentelemetry.proto.+g' {} +
-make gen-go
-mv gen/go/github.com/influxdata/influxdb-observability/otlp "$BASEDIR"/
+mkdir gen
+find . -type f -name '*.proto' -exec protoc --proto_path=. --go_out=gen {} +
+mv gen/github.com/influxdata/influxdb-observability/otlp "$BASEDIR"/
 cd "$BASEDIR"/otlp
 go mod init github.com/influxdata/influxdb-observability/otlp
+go mod tidy
--- otlp/go.mod
+++ otlp/go.mod
@@ -1,3 +1,5 @@
 module github.com/influxdata/influxdb-observability/otlp
 
 go 1.15
+
+require google.golang.org/protobuf v1.26.0
