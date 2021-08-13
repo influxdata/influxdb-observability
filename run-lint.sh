d1458f7b5d0acca9640f89a67c2e7a58386f5ac8
Jacob Marble <jacobmarble@influxdata.com>
2021-08-12 17:00:33 -0700

    chore: tweak new CI config

--- run-lint.sh
+++ run-lint.sh
@@ -5,12 +5,29 @@ set -e
 cd "$(dirname "$0")"
 BASEDIR=$(pwd)
 
+cd "${BASEDIR}/common"
+if ! go vet ; then
+  fail=1
+fi
+
+cd "${BASEDIR}/influx2otel"
+if ! go vet ; then
+  fail=1
+fi
+
+cd "${BASEDIR}/otel2influx"
+if ! go vet ; then
+  fail=1
+fi
+
 unformatted=$(gofmt -s -l "${BASEDIR}/common" "${BASEDIR}/influx2otel" "${BASEDIR}/otel2influx")
-if [ -z "$unformatted" ] ; then
-  exit 0
-else
+if [ ! -z "$unformatted" ] ; then
   for filename in $unformatted ; do
     gofmt -s -d "$filename"
   done
+  fail=1
+fi
+
+if [ -n "$fail" ] ; then
   exit 1
 fi
