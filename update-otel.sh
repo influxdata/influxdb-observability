ec616960892570eab72ea3b644073d012886a086
Jacob Marble <jacobmarble@influxdata.com>
2023-01-17 11:02:09 -0800

    chore: Small improvements to update-otel.sh

--- update-otel.sh
+++ update-otel.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 cd "$(dirname "$0")"
 BASEDIR=$(pwd)
 
 for module in common influx2otel otel2influx tests-integration; do
-  echo $module
   cd ${BASEDIR}/${module}
   go list -f '{{range .Imports}}{{.}}
 {{end}}
 {{range .TestImports}}{{.}}
-{{end}}' | sort | uniq | grep 'github.com/open-telemetry\|go.opentelemetry.io' | xargs go get -t
+{{end}}' ./... | sort | uniq | grep 'github.com/open-telemetry\|go.opentelemetry.io' | xargs go get -t
 
 done
 
