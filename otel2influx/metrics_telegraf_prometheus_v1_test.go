91826f2357f26a42aebbb94685a6d37bd107fa5f
Garry Cairns <garryjcairns@gmail.com>
2023-05-26 16:06:31 +0100

    feat(otel2influx): Adds support for DELTA temporality metrics

--- otel2influx/metrics_telegraf_prometheus_v1_test.go
+++ otel2influx/metrics_telegraf_prometheus_v1_test.go
@@ -80,259 +80,271 @@ func TestWriteMetric_v1_gauge(t *testing.T) {
 }
 
 func TestWriteMetric_v1_gaugeFromSum(t *testing.T) {
-	w := new(MockInfluxWriter)
-	c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
-	require.NoError(t, err)
+	temporalities := [2]pmetric.AggregationTemporality{pmetric.AggregationTemporalityCumulative, pmetric.AggregationTemporalityDelta}
+	for _, temporality := range temporalities {
+		w := new(MockInfluxWriter)
+		c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
+		require.NoError(t, err)
 
-	metrics := pmetric.NewMetrics()
-	rm := metrics.ResourceMetrics().AppendEmpty()
-	rm.Resource().Attributes().PutStr("container.name", "42")
-	ilMetrics := rm.ScopeMetrics().AppendEmpty()
-	ilMetrics.Scope().SetName("My Library")
-	ilMetrics.Scope().SetVersion("latest")
-	m := ilMetrics.Metrics().AppendEmpty()
-	m.SetName("cache_age_seconds")
-	m.SetDescription("Age in seconds of the current cache")
-	m.SetEmptySum()
-	m.Sum().SetIsMonotonic(false)
-	m.Sum().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
-	dp := m.Sum().DataPoints().AppendEmpty()
-	dp.Attributes().PutInt("engine_id", 0)
-	dp.SetStartTimestamp(startTimestamp)
-	dp.SetTimestamp(timestamp)
-	dp.SetDoubleValue(23.9)
-	dp = m.Sum().DataPoints().AppendEmpty()
-	dp.Attributes().PutInt("engine_id", 1)
-	dp.SetStartTimestamp(startTimestamp)
-	dp.SetTimestamp(timestamp)
-	dp.SetDoubleValue(11.9)
+		metrics := pmetric.NewMetrics()
+		rm := metrics.ResourceMetrics().AppendEmpty()
+		rm.Resource().Attributes().PutStr("container.name", "42")
+		ilMetrics := rm.ScopeMetrics().AppendEmpty()
+		ilMetrics.Scope().SetName("My Library")
+		ilMetrics.Scope().SetVersion("latest")
+		m := ilMetrics.Metrics().AppendEmpty()
+		m.SetName("cache_age_seconds")
+		m.SetDescription("Age in seconds of the current cache")
+		m.SetEmptySum()
+		m.Sum().SetIsMonotonic(false)
+		m.Sum().SetAggregationTemporality(temporality)
+		dp := m.Sum().DataPoints().AppendEmpty()
+		dp.Attributes().PutInt("engine_id", 0)
+		dp.SetStartTimestamp(startTimestamp)
+		dp.SetTimestamp(timestamp)
+		dp.SetDoubleValue(23.9)
+		dp = m.Sum().DataPoints().AppendEmpty()
+		dp.Attributes().PutInt("engine_id", 1)
+		dp.SetStartTimestamp(startTimestamp)
+		dp.SetTimestamp(timestamp)
+		dp.SetDoubleValue(11.9)
 
-	err = c.WriteMetrics(context.Background(), metrics)
-	require.NoError(t, err)
+		err = c.WriteMetrics(context.Background(), metrics)
+		require.NoError(t, err)
 
-	expected := []mockPoint{
-		{
-			measurement: "cache_age_seconds",
-			tags: map[string]string{
-				"container.name":       "42",
-				"otel.library.name":    "My Library",
-				"otel.library.version": "latest",
-				"engine_id":            "0",
+		expected := []mockPoint{
+			{
+				measurement: "cache_age_seconds",
+				tags: map[string]string{
+					"container.name":       "42",
+					"otel.library.name":    "My Library",
+					"otel.library.version": "latest",
+					"engine_id":            "0",
+				},
+				fields: map[string]interface{}{
+					common.AttributeStartTimeUnixNano: int64(startTimestamp),
+					"gauge":                           float64(23.9),
+				},
+				ts:    time.Unix(0, 1395066363000000123).UTC(),
+				vType: common.InfluxMetricValueTypeGauge,
 			},
-			fields: map[string]interface{}{
-				common.AttributeStartTimeUnixNano: int64(startTimestamp),
-				"gauge":                           float64(23.9),
+			{
+				measurement: "cache_age_seconds",
+				tags: map[string]string{
+					"container.name":       "42",
+					"otel.library.name":    "My Library",
+					"otel.library.version": "latest",
+					"engine_id":            "1",
+				},
+				fields: map[string]interface{}{
+					common.AttributeStartTimeUnixNano: int64(startTimestamp),
+					"gauge":                           float64(11.9),
+				},
+				ts:    time.Unix(0, 1395066363000000123).UTC(),
+				vType: common.InfluxMetricValueTypeGauge,
 			},
-			ts:    time.Unix(0, 1395066363000000123).UTC(),
-			vType: common.InfluxMetricValueTypeGauge,
-		},
-		{
-			measurement: "cache_age_seconds",
-			tags: map[string]string{
-				"container.name":       "42",
-				"otel.library.name":    "My Library",
-				"otel.library.version": "latest",
-				"engine_id":            "1",
-			},
-			fields: map[string]interface{}{
-				common.AttributeStartTimeUnixNano: int64(startTimestamp),
-				"gauge":                           float64(11.9),
-			},
-			ts:    time.Unix(0, 1395066363000000123).UTC(),
-			vType: common.InfluxMetricValueTypeGauge,
-		},
+		}
+
+		assert.Equal(t, expected, w.points)
 	}
-
-	assert.Equal(t, expected, w.points)
 }
 
 func TestWriteMetric_v1_sum(t *testing.T) {
-	w := new(MockInfluxWriter)
-	c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
-	require.NoError(t, err)
+	temporalities := [2]pmetric.AggregationTemporality{pmetric.AggregationTemporalityCumulative, pmetric.AggregationTemporalityDelta}
 
-	metrics := pmetric.NewMetrics()
-	rm := metrics.ResourceMetrics().AppendEmpty()
-	rm.Resource().Attributes().PutStr("container.name", "42")
-	ilMetrics := rm.ScopeMetrics().AppendEmpty()
-	ilMetrics.Scope().SetName("My Library")
-	ilMetrics.Scope().SetVersion("latest")
-	m := ilMetrics.Metrics().AppendEmpty()
-	m.SetName("http_requests_total")
-	m.SetDescription("The total number of HTTP requests")
-	m.SetEmptySum()
-	m.Sum().SetIsMonotonic(true)
-	m.Sum().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
-	dp := m.Sum().DataPoints().AppendEmpty()
-	dp.Attributes().PutInt("code", 200)
-	dp.Attributes().PutStr("method", "post")
-	dp.SetStartTimestamp(startTimestamp)
-	dp.SetTimestamp(timestamp)
-	dp.SetDoubleValue(1027)
-	dp = m.Sum().DataPoints().AppendEmpty()
-	dp.Attributes().PutInt("code", 400)
-	dp.Attributes().PutStr("method", "post")
-	dp.SetStartTimestamp(startTimestamp)
-	dp.SetTimestamp(timestamp)
-	dp.SetDoubleValue(3)
+	for _, temporality := range temporalities {
+		w := new(MockInfluxWriter)
+		c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
+		require.NoError(t, err)
+		metrics := pmetric.NewMetrics()
+		rm := metrics.ResourceMetrics().AppendEmpty()
+		rm.Resource().Attributes().PutStr("container.name", "42")
+		ilMetrics := rm.ScopeMetrics().AppendEmpty()
+		ilMetrics.Scope().SetName("My Library")
+		ilMetrics.Scope().SetVersion("latest")
+		m := ilMetrics.Metrics().AppendEmpty()
+		m.SetName("http_requests_total")
+		m.SetDescription("The total number of HTTP requests")
+		m.SetEmptySum()
+		m.Sum().SetIsMonotonic(true)
+		m.Sum().SetAggregationTemporality(temporality)
+		dp := m.Sum().DataPoints().AppendEmpty()
+		dp.Attributes().PutInt("code", 200)
+		dp.Attributes().PutStr("method", "post")
+		dp.SetStartTimestamp(startTimestamp)
+		dp.SetTimestamp(timestamp)
+		dp.SetDoubleValue(1027)
+		dp = m.Sum().DataPoints().AppendEmpty()
+		dp.Attributes().PutInt("code", 400)
+		dp.Attributes().PutStr("method", "post")
+		dp.SetStartTimestamp(startTimestamp)
+		dp.SetTimestamp(timestamp)
+		dp.SetDoubleValue(3)
 
-	err = c.WriteMetrics(context.Background(), metrics)
-	require.NoError(t, err)
+		err = c.WriteMetrics(context.Background(), metrics)
+		require.NoError(t, err)
 
-	expected := []mockPoint{
-		{
-			measurement: "http_requests_total",
-			tags: map[string]string{
-				"container.name":       "42",
-				"otel.library.name":    "My Library",
-				"otel.library.version": "latest",
-				"method":               "post",
-				"code":                 "200",
+		expected := []mockPoint{
+			{
+				measurement: "http_requests_total",
+				tags: map[string]string{
+					"container.name":       "42",
+					"otel.library.name":    "My Library",
+					"otel.library.version": "latest",
+					"method":               "post",
+					"code":                 "200",
+				},
+				fields: map[string]interface{}{
+					common.AttributeStartTimeUnixNano: int64(startTimestamp),
+					"counter":                         float64(1027),
+				},
+				ts:    time.Unix(0, 1395066363000000123).UTC(),
+				vType: common.InfluxMetricValueTypeSum,
 			},
-			fields: map[string]interface{}{
-				common.AttributeStartTimeUnixNano: int64(startTimestamp),
-				"counter":                         float64(1027),
+			{
+				measurement: "http_requests_total",
+				tags: map[string]string{
+					"container.name":       "42",
+					"otel.library.name":    "My Library",
+					"otel.library.version": "latest",
+					"method":               "post",
+					"code":                 "400",
+				},
+				fields: map[string]interface{}{
+					common.AttributeStartTimeUnixNano: int64(startTimestamp),
+					"counter":                         float64(3),
+				},
+				ts:    time.Unix(0, 1395066363000000123).UTC(),
+				vType: common.InfluxMetricValueTypeSum,
 			},
-			ts:    time.Unix(0, 1395066363000000123).UTC(),
-			vType: common.InfluxMetricValueTypeSum,
-		},
-		{
-			measurement: "http_requests_total",
-			tags: map[string]string{
-				"container.name":       "42",
-				"otel.library.name":    "My Library",
-				"otel.library.version": "latest",
-				"method":               "post",
-				"code":                 "400",
-			},
-			fields: map[string]interface{}{
-				common.AttributeStartTimeUnixNano: int64(startTimestamp),
-				"counter":                         float64(3),
-			},
-			ts:    time.Unix(0, 1395066363000000123).UTC(),
-			vType: common.InfluxMetricValueTypeSum,
-		},
+		}
+
+		assert.Equal(t, expected, w.points)
 	}
-
-	assert.Equal(t, expected, w.points)
 }
 
 func TestWriteMetric_v1_histogram(t *testing.T) {
-	w := new(MockInfluxWriter)
-	c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
-	require.NoError(t, err)
+	temporalities := [2]pmetric.AggregationTemporality{pmetric.AggregationTemporalityCumulative, pmetric.AggregationTemporalityDelta}
+	for _, temporality := range temporalities {
+		w := new(MockInfluxWriter)
+		c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
+		require.NoError(t, err)
 
-	metrics := pmetric.NewMetrics()
-	rm := metrics.ResourceMetrics().AppendEmpty()
-	rm.Resource().Attributes().PutStr("container.name", "42")
-	ilMetrics := rm.ScopeMetrics().AppendEmpty()
-	ilMetrics.Scope().SetName("My Library")
-	ilMetrics.Scope().SetVersion("latest")
-	m := ilMetrics.Metrics().AppendEmpty()
-	m.SetName("http_request_duration_seconds")
-	m.SetEmptyHistogram()
-	m.SetDescription("A histogram of the request duration")
-	m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
-	dp := m.Histogram().DataPoints().AppendEmpty()
-	dp.Attributes().PutInt("code", 200)
-	dp.Attributes().PutStr("method", "post")
-	dp.SetStartTimestamp(startTimestamp)
-	dp.SetTimestamp(timestamp)
-	dp.SetCount(144320)
-	dp.SetSum(53423)
-	dp.SetMin(0)
-	dp.SetMax(100)
-	dp.BucketCounts().FromRaw([]uint64{24054, 9390, 66948, 28997, 4599, 10332})
-	dp.ExplicitBounds().FromRaw([]float64{0.05, 0.1, 0.2, 0.5, 1})
+		metrics := pmetric.NewMetrics()
+		rm := metrics.ResourceMetrics().AppendEmpty()
+		rm.Resource().Attributes().PutStr("container.name", "42")
+		ilMetrics := rm.ScopeMetrics().AppendEmpty()
+		ilMetrics.Scope().SetName("My Library")
+		ilMetrics.Scope().SetVersion("latest")
+		m := ilMetrics.Metrics().AppendEmpty()
+		m.SetName("http_request_duration_seconds")
+		m.SetEmptyHistogram()
+		m.SetDescription("A histogram of the request duration")
+		m.Histogram().SetAggregationTemporality(temporality)
+		dp := m.Histogram().DataPoints().AppendEmpty()
+		dp.Attributes().PutInt("code", 200)
+		dp.Attributes().PutStr("method", "post")
+		dp.SetStartTimestamp(startTimestamp)
+		dp.SetTimestamp(timestamp)
+		dp.SetCount(144320)
+		dp.SetSum(53423)
+		dp.SetMin(0)
+		dp.SetMax(100)
+		dp.BucketCounts().FromRaw([]uint64{24054, 9390, 66948, 28997, 4599, 10332})
+		dp.ExplicitBounds().FromRaw([]float64{0.05, 0.1, 0.2, 0.5, 1})
 
-	err = c.WriteMetrics(context.Background(), metrics)
-	require.NoError(t, err)
+		err = c.WriteMetrics(context.Background(), metrics)
+		require.NoError(t, err)
 
-	expected := []mockPoint{
-		{
-			measurement: "http_request_duration_seconds",
-			tags: map[string]string{
-				"container.name":       "42",
-				"otel.library.name":    "My Library",
-				"otel.library.version": "latest",
-				"method":               "post",
-				"code":                 "200",
+		expected := []mockPoint{
+			{
+				measurement: "http_request_duration_seconds",
+				tags: map[string]string{
+					"container.name":       "42",
+					"otel.library.name":    "My Library",
+					"otel.library.version": "latest",
+					"method":               "post",
+					"code":                 "200",
+				},
+				fields: map[string]interface{}{
+					common.AttributeStartTimeUnixNano: int64(startTimestamp),
+					"count":                           float64(144320),
+					"sum":                             float64(53423),
+					"0.05":                            float64(24054),
+					"0.1":                             float64(33444),
+					"0.2":                             float64(100392),
+					"0.5":                             float64(129389),
+					"1":                               float64(133988),
+					"min":                             float64(0),
+					"max":                             float64(100),
+				},
+				ts:    time.Unix(0, 1395066363000000123).UTC(),
+				vType: common.InfluxMetricValueTypeHistogram,
 			},
-			fields: map[string]interface{}{
-				common.AttributeStartTimeUnixNano: int64(startTimestamp),
-				"count":                           float64(144320),
-				"sum":                             float64(53423),
-				"0.05":                            float64(24054),
-				"0.1":                             float64(33444),
-				"0.2":                             float64(100392),
-				"0.5":                             float64(129389),
-				"1":                               float64(133988),
-				"min":                             float64(0),
-				"max":                             float64(100),
-			},
-			ts:    time.Unix(0, 1395066363000000123).UTC(),
-			vType: common.InfluxMetricValueTypeHistogram,
-		},
+		}
+
+		assert.Equal(t, expected, w.points)
 	}
-
-	assert.Equal(t, expected, w.points)
 }
 
 func TestWriteMetric_v1_histogram_missingInfinityBucket(t *testing.T) {
-	w := new(MockInfluxWriter)
-	c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
-	require.NoError(t, err)
+	temporalities := [2]pmetric.AggregationTemporality{pmetric.AggregationTemporalityCumulative, pmetric.AggregationTemporalityDelta}
+	for _, temporality := range temporalities {
+		w := new(MockInfluxWriter)
+		c, err := otel2influx.NewOtelMetricsToLineProtocol(new(common.NoopLogger), w, common.MetricsSchemaTelegrafPrometheusV1)
+		require.NoError(t, err)
 
-	metrics := pmetric.NewMetrics()
-	rm := metrics.ResourceMetrics().AppendEmpty()
-	rm.Resource().Attributes().PutStr("container.name", "42")
-	ilMetrics := rm.ScopeMetrics().AppendEmpty()
-	ilMetrics.Scope().SetName("My Library")
-	ilMetrics.Scope().SetVersion("latest")
-	m := ilMetrics.Metrics().AppendEmpty()
-	m.SetName("http_request_duration_seconds")
-	m.SetEmptyHistogram()
-	m.SetDescription("A histogram of the request duration")
-	m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
-	dp := m.Histogram().DataPoints().AppendEmpty()
-	dp.Attributes().PutInt("code", 200)
-	dp.Attributes().PutStr("method", "post")
-	dp.SetStartTimestamp(startTimestamp)
-	dp.SetTimestamp(timestamp)
-	dp.SetCount(144320)
-	dp.SetSum(53423)
-	dp.BucketCounts().FromRaw([]uint64{24054, 9390, 66948, 28997, 4599, 10332})
-	dp.ExplicitBounds().FromRaw([]float64{0.05, 0.1, 0.2, 0.5, 1})
+		metrics := pmetric.NewMetrics()
+		rm := metrics.ResourceMetrics().AppendEmpty()
+		rm.Resource().Attributes().PutStr("container.name", "42")
+		ilMetrics := rm.ScopeMetrics().AppendEmpty()
+		ilMetrics.Scope().SetName("My Library")
+		ilMetrics.Scope().SetVersion("latest")
+		m := ilMetrics.Metrics().AppendEmpty()
+		m.SetName("http_request_duration_seconds")
+		m.SetEmptyHistogram()
+		m.SetDescription("A histogram of the request duration")
+		m.Histogram().SetAggregationTemporality(temporality)
+		dp := m.Histogram().DataPoints().AppendEmpty()
+		dp.Attributes().PutInt("code", 200)
+		dp.Attributes().PutStr("method", "post")
+		dp.SetStartTimestamp(startTimestamp)
+		dp.SetTimestamp(timestamp)
+		dp.SetCount(144320)
+		dp.SetSum(53423)
+		dp.BucketCounts().FromRaw([]uint64{24054, 9390, 66948, 28997, 4599, 10332})
+		dp.ExplicitBounds().FromRaw([]float64{0.05, 0.1, 0.2, 0.5, 1})
 
-	err = c.WriteMetrics(context.Background(), metrics)
-	require.NoError(t, err)
+		err = c.WriteMetrics(context.Background(), metrics)
+		require.NoError(t, err)
 
-	expected := []mockPoint{
-		{
-			measurement: "http_request_duration_seconds",
-			tags: map[string]string{
-				"container.name":       "42",
-				"otel.library.name":    "My Library",
-				"otel.library.version": "latest",
-				"method":               "post",
-				"code":                 "200",
+		expected := []mockPoint{
+			{
+				measurement: "http_request_duration_seconds",
+				tags: map[string]string{
+					"container.name":       "42",
+					"otel.library.name":    "My Library",
+					"otel.library.version": "latest",
+					"method":               "post",
+					"code":                 "200",
+				},
+				fields: map[string]interface{}{
+					common.AttributeStartTimeUnixNano: int64(startTimestamp),
+					"count":                           float64(144320),
+					"sum":                             float64(53423),
+					"0.05":                            float64(24054),
+					"0.1":                             float64(33444),
+					"0.2":                             float64(100392),
+					"0.5":                             float64(129389),
+					"1":                               float64(133988),
+				},
+				ts:    time.Unix(0, 1395066363000000123).UTC(),
+				vType: common.InfluxMetricValueTypeHistogram,
 			},
-			fields: map[string]interface{}{
-				common.AttributeStartTimeUnixNano: int64(startTimestamp),
-				"count":                           float64(144320),
-				"sum":                             float64(53423),
-				"0.05":                            float64(24054),
-				"0.1":                             float64(33444),
-				"0.2":                             float64(100392),
-				"0.5":                             float64(129389),
-				"1":                               float64(133988),
-			},
-			ts:    time.Unix(0, 1395066363000000123).UTC(),
-			vType: common.InfluxMetricValueTypeHistogram,
-		},
+		}
+
+		assert.Equal(t, expected, w.points)
 	}
-
-	assert.Equal(t, expected, w.points)
 }
 
 func TestWriteMetric_v1_summary(t *testing.T) {
