c72e04d2fffdbf631076ce7095fc0554958be4ec
Jacob Marble <jacobmarble@gmail.com>
2023-06-08 12:56:00 -0700

    chore(otelcol-influxdb): update OpenTelemetry to v0.79.0
    Also improves checks.

--- run-checks.sh
+++ run-checks.sh
@@ -16,6 +16,13 @@ if ! hash staticcheck; then
     exit 1
   fi
 fi
+if ! hash builder; then
+  echo "installing the opentelemetry collector builder"
+  if ! go install go.opentelemetry.io/collector/cmd/builder@v0.79.0; then
+    echo "failed to install the opentelemetry collector builder"
+    exit 1
+  fi
+fi
 
 for package in common influx2otel otel2influx jaeger-influxdb tests-integration; do
   echo checking ${package}
@@ -41,6 +48,12 @@ for package in common influx2otel otel2influx jaeger-influxdb tests-integration;
     fail=1
   fi
 done
+
+echo checking otelcol-influxdb
+cd "${BASEDIR}/otelcol-influxdb"
+if ! builder --config build.yml; then
+  fail=1
+fi
 
 echo
 
