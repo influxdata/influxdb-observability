d6da689ca34157387d20d478cb47eb8bb32b2634
Martin Hilton <mhilton@influxdata.com>
2025-06-13 14:50:16 +0100

    build(ci): Update to supported go versions (#328)
    * build(ci): Update to supported go versions
    
    Update the go versions used in CI to the currently supported versions
    1.23 ans 1.24. This will allow dependabot to update dependencies.
    
    * fix: update staticcheck version
    
    * fix(jaeger-influxdb): stop using anonymous context key
    
    Make staticcheck happy by addressing the use of an empty anonymous
    struct as a context key, this prevents possible collisions.

--- jaeger-influxdb/internal/logctx.go
+++ jaeger-influxdb/internal/logctx.go
@@ -6,13 +6,13 @@ import (
 	"go.uber.org/zap"
 )
 
-var loggerContext struct{}
+type loggerContext struct{}
 
 func LoggerWithContext(ctx context.Context, logger *zap.Logger) context.Context {
-	return context.WithValue(ctx, loggerContext, logger)
+	return context.WithValue(ctx, loggerContext{}, logger)
 }
 
 func LoggerFromContext(ctx context.Context) *zap.Logger {
-	logger, _ := ctx.Value(loggerContext).(*zap.Logger)
+	logger, _ := ctx.Value(loggerContext{}).(*zap.Logger)
 	return logger
 }
